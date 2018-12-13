d3233064d6165cae95eabdcb44354a2e32289dbe
Carter Ison <carter@isons.org>
2018-12-13 11:23:23 -0700

    increase comment readability

--- 3a.py
+++ 3a.py
@@ -1356,8 +1356,9 @@ for thing in raw:
     cuts.insert(n, re.split("#|@|,|:|x|\s", thing)) #parse list into nested list
     del cuts[n][6], cuts[n][3], cuts[n][2], cuts[n][0] #delete empty indexes
     n += 1
-        #sample of final output: cuts[0] == ['1', '286', '440', '19', '24']
-        #                                   [id, x, y, width, height]
+        #sample of final output: cuts[0] == ['1',      '286',    '440',    '19' ,    '24'  ]
+        #                                   [id ,      x    ,    y    ,    width,    height]
+        #                                   [0  ,      1    ,    2    ,    3    ,    4     ]
 
 fabric = [[0 for y in range(2000)] for x in range(2000)] #create 2d list structure
 
@@ -1370,7 +1371,7 @@ for prop in cuts: #iterate through each proposal
     height = int(prop[4])
 
     for n2 in range(width): #iterate through x values
-        for n3 in range(height): #iterate through every y value each x value
+        for n3 in range(height): #iterate through every y value for each x value
             fabric[x][y] += 1
             if fabric[x][y] > 1:
                 overlap += 1
